a6d8f788a744f22cc9763239ae92e8f14e9b084b
mpavot <mpavot@a3c249ba-b837-0410-a99a-8c7cbf76920c>
2007-10-28 16:43:54 +0000

    Remove useless macros

--- trunk/src/ario-main.c
+++ trunk/src/ario-main.c
@@ -18,7 +18,7 @@
  */
 
 #include <config.h>
-
+#include <libintl.h>
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
 #include <curl/curl.h>
@@ -27,29 +27,6 @@
 #include "ario-util.h"
 #include "ario-debug.h"
 
-/*
- * Standard gettext macros.
- */
-#ifdef ENABLE_NLS
-#  include <libintl.h>
-#  undef _
-#  define _(String) dgettext (PACKAGE, String)
-#  define Q_(String) g_strip_context ((String), gettext (String))
-#  ifdef gettext_noop
-#    define N_(String) gettext_noop (String)
-#  else
-#    define N_(String) (String)
-#  endif
-#else
-#  define textdomain(String) (String)
-#  define gettext(String) (String)
-#  define dgettext(Domain,Message) (Message)
-#  define dcgettext(Domain,Message,Type) (Message)
-#  define bindtextdomain(Domain,Directory) (Domain)
-#  define _(String) (String)
-#  define Q_(String) g_strip_context ((String), (String))
-#  define N_(String) (String)
-#endif
 
 int
 main (int argc, char *argv[])
