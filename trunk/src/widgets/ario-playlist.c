e7162d7b06de2ad3972e851928fe926554ba11af
mpavot <mpavot@a3c249ba-b837-0410-a99a-8c7cbf76920c>
2008-07-12 15:49:53 +0000

    [ 2016602 ] Play on enter

--- trunk/src/widgets/ario-playlist.c
+++ trunk/src/widgets/ario-playlist.c
@@ -1329,10 +1329,38 @@ ario_playlist_view_key_press_cb (GtkWidget *widget,
                                  ArioPlaylist *playlist)
 {
         ARIO_LOG_FUNCTION_START
+        GList *paths;
+        GtkTreePath *path = NULL;
+        GtkTreeModel *model = GTK_TREE_MODEL (playlist->priv->model);
+
         if (event->keyval == GDK_Delete)
                 ario_playlist_remove (playlist);
 
-        return FALSE;        
+        ARIO_LOG_FUNCTION_START
+        GtkTreeIter iter;
+
+        /* get the iter from the path */
+        if (gtk_tree_model_get_iter (GTK_TREE_MODEL (playlist->priv->model), &iter, path)) {
+                gint id;
+                /* get the song id */
+                gtk_tree_model_get (GTK_TREE_MODEL (playlist->priv->model), &iter, ID_COLUMN, &id, -1);
+                ario_mpd_do_play_id (playlist->priv->mpd, id);
+        }
+        
+        if (event->keyval == GDK_Return) {
+                paths = gtk_tree_selection_get_selected_rows (playlist->priv->selection, &model);
+                if (paths)
+                        path = paths->data;
+                if (path)
+                        ario_playlist_activate_row (playlist, path);
+
+                g_list_foreach (paths, (GFunc) gtk_tree_path_free, NULL);
+                g_list_free (paths);
+
+                return TRUE;
+        }
+
+        return FALSE;
 }
 
 static gboolean
