f0ab91fdf5934dca6667bd522241c22a2e3d8902
mpavot <mpavot@a3c249ba-b837-0410-a99a-8c7cbf76920c>
2008-10-18 07:47:14 +0000

    2144909         Build hanging (audioscrobbler) on 64 bit Linux

---
+++ trunk/src/ario-enum-types.c
@@ -0,0 +1,92 @@
+
+/* Generated data (by glib-mkenums) */
+
+#include "ario-enum-types.h"
+
+/* enumerations from "servers/ario-server.h" */
+#include "servers/ario-server.h"
+
+GType
+ario_server_type_get_type (void)
+{
+	static GType the_type = 0;
+	
+	if (the_type == 0)
+	{
+		static const GEnumValue values[] = {
+			{ ArioServerMpd,
+			  "ArioServerMpd",
+			  "mpd" },
+			{ ArioServerXmms,
+			  "ArioServerXmms",
+			  "xmms" },
+			{ 0, NULL, NULL }
+		};
+		the_type = g_enum_register_static (
+				g_intern_static_string ("ArioServerType"),
+				values);
+	}
+	return the_type;
+}
+
+GType
+ario_server_action_type_get_type (void)
+{
+	static GType the_type = 0;
+	
+	if (the_type == 0)
+	{
+		static const GEnumValue values[] = {
+			{ ARIO_SERVER_ACTION_ADD,
+			  "ARIO_SERVER_ACTION_ADD",
+			  "add" },
+			{ ARIO_SERVER_ACTION_DELETE_ID,
+			  "ARIO_SERVER_ACTION_DELETE_ID",
+			  "delete-id" },
+			{ ARIO_SERVER_ACTION_DELETE_POS,
+			  "ARIO_SERVER_ACTION_DELETE_POS",
+			  "delete-pos" },
+			{ ARIO_SERVER_ACTION_MOVE,
+			  "ARIO_SERVER_ACTION_MOVE",
+			  "move" },
+			{ 0, NULL, NULL }
+		};
+		the_type = g_enum_register_static (
+				g_intern_static_string ("ArioServerActionType"),
+				values);
+	}
+	return the_type;
+}
+
+/* enumerations from "shell/ario-shell.h" */
+#include "shell/ario-shell.h"
+
+GType
+ario_visibility_get_type (void)
+{
+	static GType the_type = 0;
+	
+	if (the_type == 0)
+	{
+		static const GEnumValue values[] = {
+			{ VISIBILITY_HIDDEN,
+			  "VISIBILITY_HIDDEN",
+			  "hidden" },
+			{ VISIBILITY_VISIBLE,
+			  "VISIBILITY_VISIBLE",
+			  "visible" },
+			{ VISIBILITY_TOGGLE,
+			  "VISIBILITY_TOGGLE",
+			  "toggle" },
+			{ 0, NULL, NULL }
+		};
+		the_type = g_enum_register_static (
+				g_intern_static_string ("ArioVisibility"),
+				values);
+	}
+	return the_type;
+}
+
+
+/* Generated data ends here */
+
---
+++ trunk/src/ario-enum-types.h
@@ -0,0 +1,29 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __ARIO_ENUM_TYPES_H__
+#define __ARIO_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* Enumerations from "servers/ario-server.h" */
+
+#define ARIO_TYPE_SERVER_TYPE	(ario_server_type_get_type())
+GType ario_server_type_get_type	(void) G_GNUC_CONST;
+
+#define ARIO_TYPE_SERVER_ACTION_TYPE	(ario_server_action_type_get_type())
+GType ario_server_action_type_get_type	(void) G_GNUC_CONST;
+
+/* Enumerations from "shell/ario-shell.h" */
+
+#define ARIO_TYPE_VISIBILITY	(ario_visibility_get_type())
+GType ario_visibility_get_type	(void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* __ARIO_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+
