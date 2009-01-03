329d03dfb4225fd7e81797d3000d8c4bed2456eb
mpavot <m.pavot@gmail.com>
2009-01-03 00:36:42 +0000

    Cosmetic

--- src/plugins/ario-plugin.c
+++ src/plugins/ario-plugin.c
@@ -109,7 +109,6 @@ ario_plugin_get_plugin_paths (void)
         path = g_build_filename (ario_util_config_dir (), "plugins", NULL);
         paths = g_slist_append (paths, path);
 
-
         path = g_strdup (ARIO_PLUGIN_DIR);
         paths = g_slist_append (paths, path);
 
