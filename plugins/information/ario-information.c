147f84ce5d9b42f45090387cbea6120aa9292fc3
mpavot <m.pavot@gmail.com>
2009-04-15 21:49:03 +0000

    Cosmetic + add comments

--- plugins/information/ario-information.c
+++ plugins/information/ario-information.c
@@ -22,14 +22,14 @@
 #include <string.h>
 #include <config.h>
 #include <glib/gi18n.h>
-#include "ario-util.h"
+
 #include "ario-debug.h"
-#include "plugins/ario-plugin.h"
+#include "ario-util.h"
+#include "covers/ario-cover.h"
 #include "covers/ario-cover-handler.h"
-#include "widgets/ario-playlist.h"
 #include "lib/gtk-builder-helpers.h"
 #include "lyrics/ario-lyrics.h"
-#include "covers/ario-cover.h"
+#include "plugins/ario-plugin.h"
 #include "servers/ario-server.h"
 
 static void ario_information_finalize (GObject *object);
@@ -85,10 +85,12 @@ struct ArioInformationPrivate
         gboolean selected;
 };
 
+/* Drag and drop target */
 static const GtkTargetEntry criterias_targets  [] = {
         { "text/criterias-list", 0, 0 },
 };
 
+/* Object properties */
 enum
 {
         PROP_0,
@@ -121,10 +123,16 @@ ario_information_select (ArioSource *source)
 {
         ArioInformation *information = ARIO_INFORMATION (source);
 
+        /* Fill all information when tab is selected */
         information->priv->selected = TRUE;
 
+        /* Fill song info */
         ario_information_fill_song (information);
+
+        /* Fill cover arts */
         ario_information_fill_cover (information);
+
+        /* Fill album info */
         ario_information_fill_album (information);
 }
 
@@ -133,6 +141,7 @@ ario_information_unselect (ArioSource *source)
 {
         ArioInformation *information = ARIO_INFORMATION (source);
 
+        /* Remember to be lazy until tab is selected again */
         information->priv->selected = FALSE;
 }
 
@@ -143,17 +152,19 @@ ario_information_class_init (ArioInformationClass *klass)
         GObjectClass *object_class = G_OBJECT_CLASS (klass);
         ArioSourceClass *source_class = ARIO_SOURCE_CLASS (klass);
 
+        /* GObject virtual methods */
         object_class->finalize = ario_information_finalize;
-
         object_class->set_property = ario_information_set_property;
         object_class->get_property = ario_information_get_property;
 
+        /* ArioSource virtual methods */
         source_class->get_id = ario_information_get_id;
         source_class->get_name = ario_information_get_name;
         source_class->get_icon = ario_information_get_icon;
         source_class->select = ario_information_select;
         source_class->unselect = ario_information_unselect;
 
+        /* Object properties */
         g_object_class_install_property (object_class,
                                          PROP_UI_MANAGER,
                                          g_param_spec_object ("ui-manager",
@@ -162,6 +173,7 @@ ario_information_class_init (ArioInformationClass *klass)
                                                               GTK_TYPE_UI_MANAGER,
                                                               G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
 
+        /* Private attributes */
         g_type_class_add_private (klass, sizeof (ArioInformationPrivate));
 }
 
@@ -169,6 +181,7 @@ static void ario_information_style_set_cb (GtkWidget *vbox,
                                            GtkStyle *style,
                                            GtkWidget *vp)
 {
+        /* Set white background */
         gtk_widget_modify_bg (vp, GTK_STATE_NORMAL, &(GTK_WIDGET(vbox)->style->light[GTK_STATE_NORMAL]));
 }
 
@@ -192,25 +205,31 @@ ario_information_init (ArioInformation *information)
 
         information->priv = ARIO_INFORMATION_GET_PRIVATE (information);
 
+        /* Get UI file in one of plugins directory */
         file = ario_plugin_find_file ("information.ui");
         g_return_if_fail (file);
 
+        /* Create scrolled window */
         scrolledwindow = gtk_scrolled_window_new (NULL, NULL);
         gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
         gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_SHADOW_NONE);
 
+        /* Create viewport to have white background */
         vp = gtk_viewport_new (gtk_scrolled_window_get_hadjustment (GTK_SCROLLED_WINDOW (scrolledwindow)),
                                gtk_scrolled_window_get_vadjustment (GTK_SCROLLED_WINDOW (scrolledwindow)));
 
+        /* Create UI from file */
         builder = gtk_builder_helpers_new (file, information);
         g_free (file);
 
+        /* Get main vbox */
         vbox = GTK_WIDGET (gtk_builder_get_object (builder, "vbox"));
         g_signal_connect (vbox,
                           "style-set",
                           G_CALLBACK (ario_information_style_set_cb),
                           vp);
 
+        /* Get pointers to various widgets */
         information->priv->artist_label = GTK_WIDGET (gtk_builder_get_object (builder, "artist_label"));
         information->priv->album_label = GTK_WIDGET (gtk_builder_get_object (builder, "album_label"));
         information->priv->title_label = GTK_WIDGET (gtk_builder_get_object (builder, "title_label"));
@@ -222,8 +241,10 @@ ario_information_init (ArioInformation *information)
         information->priv->albums_hbox = GTK_WIDGET (gtk_builder_get_object (builder, "albums_hbox"));
         information->priv->albums_const_label = GTK_WIDGET (gtk_builder_get_object (builder, "albums_const_label"));
 
+        /* Get text view buffer */
         information->priv->lyrics_textbuffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (information->priv->lyrics_textview));
 
+        /* Change style of some labels */
         gtk_builder_helpers_boldify_label (builder, "artist_const_label");
         gtk_builder_helpers_boldify_label (builder, "album_const_label");
         gtk_builder_helpers_boldify_label (builder, "title_const_label");
@@ -231,11 +252,13 @@ ario_information_init (ArioInformation *information)
         gtk_builder_helpers_boldify_label (builder, "albums_const_label");
         gtk_builder_helpers_boldify_label (builder, "lyrics_const_label");
 
+        /* Connect signal for current cover change */
         g_signal_connect (ario_cover_handler_get_instance (),
                           "cover_changed",
                           G_CALLBACK (ario_information_cover_changed_cb),
                           information);
 
+        /* Widgets to scrolled window */
         gtk_container_add (GTK_CONTAINER (vp), vbox);
         gtk_container_add (GTK_CONTAINER (scrolledwindow), vp);
 
@@ -245,6 +268,8 @@ ario_information_init (ArioInformation *information)
                                  information, 0);
 
         gtk_widget_show_all (scrolledwindow);
+
+        /* Add scrolled window to information */
         gtk_box_pack_start (GTK_BOX (information), scrolledwindow, TRUE, TRUE, 0);
         g_object_unref (builder);
 }
@@ -262,6 +287,7 @@ ario_information_finalize (GObject *object)
 
         g_return_if_fail (information->priv != NULL);
 
+        /* Free a few data */
         if (information->priv->albums) {
                 g_slist_foreach (information->priv->albums, (GFunc) ario_server_free_album, NULL);
                 g_slist_free (information->priv->albums);
@@ -338,9 +364,6 @@ ario_information_new (GtkUIManager *mgr)
 
         information->priv->connected = ario_server_is_connected ();
 
-        ario_information_select (ARIO_SOURCE (information));
-        ario_information_unselect (ARIO_SOURCE (information));
-
         return GTK_WIDGET (information);
 }
 
@@ -353,12 +376,15 @@ ario_information_fill_song (ArioInformation *information)
         ArioLyrics *lyrics;
         int state;
 
+        /* Do nothing if tab is not visible */
         if (!information->priv->selected)
                 return;
 
+        /* Get info on server */
         state = ario_server_get_current_state ();
         song = ario_server_get_current_song ();
 
+        /* Hide song widgets in not connected or not playing */
         if (!information->priv->connected
             || !song
             || (state != MPD_STATUS_STATE_PLAY && state != MPD_STATUS_STATE_PAUSE)) {
@@ -368,8 +394,10 @@ ario_information_fill_song (ArioInformation *information)
                 return;
         }
 
+        /* Show all song widgets */
         gtk_widget_show_all (information->priv->properties_hbox);
 
+        /* set labels contents */
         gtk_label_set_text (GTK_LABEL (information->priv->title_label), song->title);
         gtk_label_set_text (GTK_LABEL (information->priv->artist_label), song->artist);
         gtk_label_set_text (GTK_LABEL (information->priv->album_label), song->album);
@@ -377,13 +405,16 @@ ario_information_fill_song (ArioInformation *information)
         gtk_label_set_text (GTK_LABEL (information->priv->length_label), length);
         g_free (length);
 
+        /* Get lyrics */
         lyrics = ario_lyrics_get_local_lyrics (song->artist, song->title);
         if (lyrics) {
+                /* Set lyrics in text buffer */
                 gtk_text_buffer_set_text (information->priv->lyrics_textbuffer, lyrics->lyrics, -1);
                 gtk_widget_show (information->priv->lyrics_textview);
                 gtk_widget_show (information->priv->lyrics_label);
                 ario_lyrics_free (lyrics);
         } else {
+                /* Hide text view if no lyrics found */
                 gtk_widget_hide (information->priv->lyrics_textview);
                 gtk_widget_hide (information->priv->lyrics_label);
         }
@@ -395,10 +426,14 @@ ario_information_fill_cover (ArioInformation *information)
         ARIO_LOG_FUNCTION_START;
         GdkPixbuf *cover;
 
+        /* Do nothing if tab is not visible */
         if (!information->priv->selected)
                 return;
 
+        /* Get current album cover */
         cover = ario_cover_handler_get_large_cover ();
+
+        /* Set cover widget */
         gtk_image_set_from_pixbuf (GTK_IMAGE (information->priv->cover_image), cover);
 }
 
@@ -406,6 +441,7 @@ static void
 ario_information_album_foreach (GtkWidget *widget,
                                 GtkContainer *container)
 {
+        /* Remove widget */
         gtk_container_remove (container, widget);
 }
 
@@ -425,13 +461,16 @@ ario_information_fill_album (ArioInformation *information)
         int nb = 0;
         GtkWidget *event_box;
 
+        /* Do nothing if tab is not visible */
         if (!information->priv->selected)
                 return;
 
+        /* Remove all cover arts widgets */
         gtk_container_foreach (GTK_CONTAINER (information->priv->albums_hbox),
                                (GtkCallback) ario_information_album_foreach,
                                information->priv->albums_hbox);
 
+        /* Free a few data */
         if (information->priv->albums) {
                 g_slist_foreach (information->priv->albums, (GFunc) ario_server_free_album, NULL);
                 g_slist_free (information->priv->albums);
@@ -439,15 +478,18 @@ ario_information_fill_album (ArioInformation *information)
         }
         gtk_widget_hide (information->priv->albums_const_label);
 
+        /* Get info on server */
         state = ario_server_get_current_state ();
         song = ario_server_get_current_song ();
 
+        /* Stop here is not connected or not playing */
         if (!information->priv->connected
             || !song
             || (state != MPD_STATUS_STATE_PLAY && state != MPD_STATUS_STATE_PAUSE)) {
                 return;
         }
 
+        /* Get all albums of current artist */
         criteria = g_slist_append (criteria, &atomic_criteria);
         atomic_criteria.tag = MPD_TAG_ITEM_ARTIST;
         atomic_criteria.value = song->artist;
@@ -455,19 +497,24 @@ ario_information_fill_album (ArioInformation *information)
         information->priv->albums = ario_server_get_albums (criteria);
         g_slist_free (criteria);
 
+        /* For each album */
         for (tmp = information->priv->albums; tmp && nb < 8; tmp = g_slist_next (tmp)) {
                 album = tmp->data;
+                /* Do not consider current album */
                 if ((!album->album && !song->album)
                     || (album->album && song->album && !strcmp (album->album, song->album)))
                         continue;
 
+                /* Get albums cover */
                 cover_path = ario_cover_make_cover_path (album->artist, album->album, SMALL_COVER);
                 pixbuf = gdk_pixbuf_new_from_file_at_size (cover_path, COVER_SIZE, COVER_SIZE, NULL);
                 g_free (cover_path);
                 if (pixbuf) {
+                        /* Cover found: create widgets to add cover art */
                         event_box = gtk_event_box_new ();
                         image = gtk_image_new_from_pixbuf (pixbuf);
 
+                        /* Add drag and drop feature to image */
                         gtk_drag_source_set (event_box,
                                              GDK_BUTTON1_MASK,
                                              criterias_targets,
@@ -490,6 +537,7 @@ ario_information_fill_album (ArioInformation *information)
                 }
         }
 
+        /* Show albums widgets if there are some */
         if (nb > 0) {
                 gtk_widget_show_all (information->priv->albums_hbox);
                 gtk_widget_show (information->priv->albums_const_label);
@@ -502,8 +550,14 @@ ario_information_state_changed_cb (ArioServer *server,
 {
         ARIO_LOG_FUNCTION_START;
         information->priv->connected = ario_server_is_connected ();
+
+        /* Fill song info */
         ario_information_fill_song (information);
+
+        /* Fill cover arts */
         ario_information_fill_cover (information);
+
+        /* Fill album info */
         ario_information_fill_album (information);
 }
 
@@ -512,6 +566,7 @@ ario_information_song_changed_cb (ArioServer *server,
                                   ArioInformation *information)
 {
         ARIO_LOG_FUNCTION_START;
+        /* Fill song info */
         ario_information_fill_song (information);
 }
 
@@ -520,6 +575,7 @@ ario_information_cover_changed_cb (ArioCoverHandler *cover_handler,
                                    ArioInformation *information)
 {
         ARIO_LOG_FUNCTION_START;
+        /* Fill cover arts */
         ario_information_fill_cover (information);
 }
 
@@ -528,6 +584,7 @@ ario_information_album_changed_cb (ArioServer *server,
                                    ArioInformation *information)
 {
         ARIO_LOG_FUNCTION_START;
+        /* Fill album info */
         ario_information_fill_album (information);
 }
 
@@ -540,6 +597,7 @@ ario_information_cover_drag_data_get_cb (GtkWidget *widget,
         ARIO_LOG_FUNCTION_START;
         gchar *str;
 
+        /* Get drag data corresponding to dragged album */
         str = g_strdup_printf ("2\n%d\n%s\n%d\n%s\n", MPD_TAG_ITEM_ARTIST, album->artist, MPD_TAG_ITEM_ALBUM, album->album);
         gtk_selection_data_set (selection_data, selection_data->target, 8, (const guchar *) str,
                                 strlen (str) * sizeof(guchar));
@@ -559,6 +617,7 @@ ario_information_cover_button_press_cb (GtkWidget *widget,
         GSList *criterias = NULL;
 
         if (event->button == 1 && event->type == GDK_2BUTTON_PRESS) {
+                /* Double click on image */
                 atomic_criteria1.tag = MPD_TAG_ITEM_ARTIST;
                 atomic_criteria1.value = album->artist;
                 atomic_criteria2.tag = MPD_TAG_ITEM_ALBUM;
@@ -569,6 +628,7 @@ ario_information_cover_button_press_cb (GtkWidget *widget,
 
                 criterias = g_slist_append (criterias, criteria);
 
+                /* Add album to playlist */
                 ario_server_playlist_append_criterias (criterias, FALSE, -1);
 
                 g_slist_free (criteria);
