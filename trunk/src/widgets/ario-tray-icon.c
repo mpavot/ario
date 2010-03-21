d8407c4d1733df76d5bf900d32bc1c92af447f9e
mpavot <mpavot@a3c249ba-b837-0410-a99a-8c7cbf76920c>
2010-03-21 15:57:09 +0000

    Change shortcut for stop to avoid problems with "space" key

--- trunk/src/widgets/ario-tray-icon.c
+++ trunk/src/widgets/ario-tray-icon.c
@@ -105,7 +105,7 @@ static GtkActionEntry ario_tray_icon_actions [] =
         { "ControlPause", GTK_STOCK_MEDIA_PAUSE, N_("_Pause"), "<control>Down",
                 NULL,
                 G_CALLBACK (ario_tray_icon_cmd_pause) },
-        { "ControlStop", GTK_STOCK_MEDIA_STOP, N_("_Stop"), "<control>space",
+        { "ControlStop", GTK_STOCK_MEDIA_STOP, N_("_Stop"), "<control><shift>Down",
                 NULL,
                 G_CALLBACK (ario_tray_icon_cmd_stop) },
         { "ControlNext", GTK_STOCK_MEDIA_NEXT, N_("_Next"), "<control>Right",
