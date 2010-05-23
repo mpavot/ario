6356c41b6218f2cc20c986aac8b874747bfbacae
mpavot <m.pavot@gmail.com>
2010-05-23 20:02:39 +0000

    Fix bug 3002216 - Option dialog not legible in ubuntu lucid

---
+++ src/preferences/ario-taskbar-preferences.h
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2005 Marc Pavot <marc.pavot@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef __ARIO_TASKBAR_PREFERENCES_H
+#define __ARIO_TASKBAR_PREFERENCES_H
+
+#include <glib.h>
+#include <gtk/gtkvbox.h>
+
+G_BEGIN_DECLS
+
+#define TYPE_ARIO_TASKBAR_PREFERENCES         (ario_taskbar_preferences_get_type ())
+#define ARIO_TASKBAR_PREFERENCES(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), TYPE_ARIO_TASKBAR_PREFERENCES, ArioTaskbarPreferences))
+#define ARIO_TASKBAR_PREFERENCES_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), TYPE_ARIO_TASKBAR_PREFERENCES, ArioTaskbarPreferencesClass))
+#define IS_ARIO_TASKBAR_PREFERENCES(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), TYPE_ARIO_TASKBAR_PREFERENCES))
+#define IS_ARIO_TASKBAR_PREFERENCES_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), TYPE_ARIO_TASKBAR_PREFERENCES))
+#define ARIO_TASKBAR_PREFERENCES_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), TYPE_ARIO_TASKBAR_PREFERENCES, ArioTaskbarPreferencesClass))
+
+typedef struct ArioTaskbarPreferencesPrivate ArioTaskbarPreferencesPrivate;
+
+typedef struct
+{
+        GtkVBox parent;
+
+        ArioTaskbarPreferencesPrivate *priv;
+} ArioTaskbarPreferences;
+
+typedef struct
+{
+        GtkVBoxClass parent_class;
+} ArioTaskbarPreferencesClass;
+
+GType              ario_taskbar_preferences_get_type         (void) G_GNUC_CONST;
+
+GtkWidget *        ario_taskbar_preferences_new              (void);
+
+G_END_DECLS
+
+#endif /* __ARIO_TASKBAR_PREFERENCES_H */
